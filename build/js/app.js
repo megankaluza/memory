b12fa1f4c599d89fd14f4bb742bf011b4b0c0c8e
Megan Kaluza and Matt Nicholas <megan@>
2016-10-10 16:12:17 -0700

    Functioning game

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,18 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Memory(){
+}
+
+Memory.prototype.checkForMatch = function(firstGuess, secondGuess){
+  var result = false;
+  if(firstGuess === secondGuess){
+    result = true;
+  }
+  return result;
+};
+
+exports.memoryModule = Memory;
+
+},{}],2:[function(require,module,exports){
 var Memory = require('./../js/memory.js').memoryModule;
 var firstGuess;
 var secondGuess;
@@ -164,3 +179,5 @@ $(document).ready(function(){
 });
 
 exports.memoryModule = Memory;
+
+},{"./../js/memory.js":1}]},{},[2]);
--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,12 @@
-function checkForMatch(firstGuess, secondGuess){
-  if(firstGuess === secondGuess){
-    return true;
-  }
-  return false;
+function Memory(){
 }
+
+Memory.prototype.checkForMatch = function(firstGuess, secondGuess){
+  var result = false;
+  if(firstGuess === secondGuess){
+    result = true;
+  }
+  return result;
+};
+
+exports.memoryModule = Memory;
