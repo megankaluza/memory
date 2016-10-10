4872a7e1a5e7d7bfcf43389911843c23524db4be
Megan Kaluza and Matt Nicholas <megan@>
2016-10-10 11:29:25 -0700

    Add display toggle to buttons

--- js/memory-interface.js
+++ js/memory-interface.js
@@ -1,4 +1,38 @@
-$(document).ready(function(event){
-  event.preventDefault();
+$(document).ready(function(){
 
-})
+  $('#card1-back').click(function(){
+    // event.preventDefault();
+
+    $('#card1-back').toggle();
+    $('#card1-face').toggle();
+  });
+  $('#card2-back').click(function(){
+    $('#card2-back').toggle();
+    $('#card2-face').toggle();
+  });
+  $('#card3-back').click(function(){
+    $('#card3-back').toggle();
+    $('#card3-face').toggle();
+  });
+  $('#card4-back').click(function(){
+    $('#card4-back').toggle();
+    $('#card4-face').toggle();
+  });
+  $('#card5-back').click(function(){
+    $('#card5-back').toggle();
+    $('#card5-face').toggle();
+  });
+  $('#card6-back').click(function(){
+    $('#card6-back').toggle();
+    $('#card6-face').toggle();
+  });
+  $('#card7-back').click(function(){
+    $('#card7-back').toggle();
+    $('#card7-face').toggle();
+  });
+  $('#card8-back').click(function(){
+    $('#card8-back').toggle();
+    $('#card8-face').toggle();
+  });
+
+});
